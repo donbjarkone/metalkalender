4f740d6155d0079c5fd5c14fb1f636d520455a8e
donbjarkone <44224869+donbjarkone@users.noreply.github.com>
2022-10-18 14:02:10 +0200

    Added first running code

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-let got = require('got');
+import got from 'got';
+import * as cheerio from 'cheerio';
 
-console.log('Hello world');
+(async function () {
+    const url = 'https://metalkalender.dk';
+    const response = await got(url);
+
+    // console.log(response);
+
+    const $ = cheerio.load(response.body)
+
+    const result = $('p').text();
+    console.log(result);
+})();
